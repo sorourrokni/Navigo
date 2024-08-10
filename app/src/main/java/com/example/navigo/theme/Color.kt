c9891feca6abb232b267bd96014a804efff3846b
sorour <soroorrokni@gmail.com>
2024-08-10 12:47:44 +0330

    fix colors and theme

--- app/src/main/java/com/example/navigo/theme/Color.kt
+++ app/src/main/java/com/example/navigo/theme/Color.kt
@@ -2,10 +2,8 @@ package com.example.navigo.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Primary = Color(124, 185, 255, 255)
+val Secondary = Color(0xffffdc48)
+val Background = Color(255, 255, 255, 255)
+val Text = Color(0xff848484)
+val OnPrimary = Color(0xff040a0b)
--- app/src/main/java/com/example/navigo/theme/Theme.kt
+++ app/src/main/java/com/example/navigo/theme/Theme.kt
@@ -1,56 +1,42 @@
 package com.example.navigo.theme
 
-import android.os.Build
+import android.annotation.SuppressLint
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Primary,
+    secondary = Secondary,
+    onPrimary = OnPrimary,
+    background = Background,
+    surfaceTint = Text,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Primary,
+    secondary = Secondary,
+    onPrimary = OnPrimary,
+    background = Background,
+    surfaceTint = Text,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+    )
 
+@SuppressLint("NewApi")
 @Composable
 fun NavigoTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
+    val myColorScheme = when {
+        isDarkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = myColorScheme,
         typography = Typography,
         content = content
     )
